4d3b4a3a6e6746760b3a697888422805075097ea
Weaver-K <malcolmoyieke47@gmail.com>
2025-08-11 12:20:23 +0300

    Update wk2-assignment.py

--- wk2-assignment.py
+++ wk2-assignment.py
@@ -3,7 +3,7 @@ my_list.append(10)
 my_list.append(20)
 my_list.append(30)
 my_list.append(40)
-my_list. insert(1, 15)
+my_list.insert(1, 15)
 list2 = [50, 60, 70]
 my_list.extend(list2)
 del my_list[-1]
