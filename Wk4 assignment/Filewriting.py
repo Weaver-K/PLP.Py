c843d3c4f2260c04bf1b551089e570eecc2a4fea
Weaver-K <malcolmoyieke47@gmail.com>
2025-08-24 18:46:45 +0300

    done

--- Wk4 assignment/Filewriting.py
+++ Wk4 assignment/Filewriting.py
@@ -1,16 +1,15 @@
-# 
-# x = 120
-# y = 200
-# z = 300
-# def add(x, y, z):
-#     return x + y + z
-# print(add(x, y, z))
-
-# file = open("readme.md", "a", encoding="utf-8")
-# print(file.read())
-# file.close()
-
-with open("readme.md", "a", encoding="utf-8") as file: 
-    file.write("#Welcome to PLP\n")
-    
+#writing to a file
+#the code opens a file in write mode
+#the user inputs the file name
+input_file = input("Enter the file name to write to: ")
+with open(input_file, "w", encoding="utf-8") as file: 
+    file.write("#Lets learn python👾\n")
+    file.write("#This is my first file\n")
+    file.write("#This demonstrates file handling in python🐍\n")
+    file.write("#Especially file writing💾\n")
+file.close
+if file.write:
+    print("File written successfully")
+else:
+    print("File not written")    
     
--- Wk4 assignment/Try_except.py
+++ Wk4 assignment/Try_except.py
@@ -1,6 +1,11 @@
 # Error handling in python 
+#First input file to be received
+input_file = input("Enter the file name: ")
+#the code runs to read the file
 try:
-    with open("Mark1.xlsx", "r") as f: 
+    with open(input_file, "r") as f: 
      content = f.read()
 except FileNotFoundError:
-    print("The file was not found.") 
+    print("The file was not found.")
+else:
+     print("File retrieved successfully") 
